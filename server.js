9227d2908b3111c382bd3f74d6b0ec0112a453d0
jrmwill <32336362+jrmwill@users.noreply.github.com>
2021-03-05 16:04:50 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const socketIO = require('socket.io');
+
+const port = process.env.PORT || 3000;
+const index = '/index.html';
+
+const server = express()
+	.use((req,res) => {
+		res.sendFile(index, {root: __dirname})
+	})
+	.listen(port, () => {
+		console.log(`Server started on port: ${port}`);
+	});
+
+const io = socketIO(server);
+
+let users = [];
+let currentPlayer = null;
+let timeout = null;
+
+io.on('connection', (socket) => {
+	socket.on('username', (username) => {
+		console.log(`${username} joined the game.`);
+
+		socket.username = username;
+
+		users.push(socket);
+		sendUsers();
+
+		if(users.length === 1){
+			currentPlayer = socket;
+			switchPlayer();
+		}
+	});
+
+	socket.on('disconnect', () => {
+		users = users.filter((user) => {
+			return user !== socket
+		});
+		sendUsers();
+	});
+
+	socket.on('line', (data) => {
+		socket.broadcast.emit('line', data);
+	})
+});
+
+function sendUsers(){
+	const usersData = users.map((user) => {
+		return {
+			username: user.username,
+			isActive: user === currentPlayer
+		}
+	});
+	io.emit('users', usersData);
+}
+
+function switchPlayer(){
+	timeout = setTimeout(switchPlayer, 15000);
+
+	const indexCurrentPlayer = users.indexOf(currentPlayer);
+	currentPlayer = users[(indexCurrentPlayer+1) % users.length];
+
+	sendUsers();
+}	
